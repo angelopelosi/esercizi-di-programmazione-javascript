7e83dd84e619debad2d18a67b54696e83a7f1260
angelopelosi <75650476+angelopelosi@users.noreply.github.com>
2022-07-19 13:35:50 +0200

    update

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -11,3 +11,10 @@
 
   http://www.imparareaprogrammare.it
 */
+var annocorrente = 2022 ;
+var annodinascitapersona = 1995 ;
+
+var etapersona = annocorrente - annodinascitapersona ;
+var quantomancaper100anni = 100 - etapersona ;
+
+console.log(etapersona , quantomancaper100anni) ;
