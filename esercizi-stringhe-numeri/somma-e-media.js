3bebe236e5d4e159b6b3a1fe39eda8b02d5ec136
angelopelosi <75650476+angelopelosi@users.noreply.github.com>
2022-07-19 13:36:36 +0200

    update

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -12,5 +12,4 @@ var a = 1; var b = 2; var c = 3; var d = 4; var e = 5;
 var somma = a + b + c + d + e;
 var ntot = 5;
 var media = somma / ntot;
-console.log(somma)
-console.log(media)
+console.log(somma , media)
